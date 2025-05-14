c894cadf5f6c1885f69cf9c3bcc603300f035e01
Kovács Áron <kovacs.aron13@gmail.com>
2025-05-14 15:34:48 +0200

    added core count todo

---
+++ hypettpd.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+int main (int argc, char *argv[]) {
+    
+    uint32_t cpu_core_count;
+
+    /* TODO: get cpu core count */
+
+
+    return 1;
+}
